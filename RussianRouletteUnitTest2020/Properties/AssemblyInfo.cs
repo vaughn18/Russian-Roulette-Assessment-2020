a7bfd52eb33f9e5b238588cb5f4a5ff889f38d6e
vaughn18 <vaughn_mainacc@yahoo.com>
2020-04-19 23:52:53 +1200

    Unit test folder added

---
+++ RussianRouletteUnitTest2020/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RussianRouletteUnitTest2020")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RussianRouletteUnitTest2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae90ac73-a0b9-49da-9c0d-7c8cac540821")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RussianRouletteUnitTest2020/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RussianRouletteAssessment2020;
+
+namespace RussianRouletteUnitTest2020
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Battle myClass = new Battle();
+        [TestMethod]
+        public void AttackMethodTesting()
+        {
+            //Random number generator was changed to a hardcoded number for testing purposes
+            myClass.Prepare();
+
+            var Answer = myClass.Attack("4");
+
+            Assert.AreEqual("Your attack failed", Answer);
+        }
+
+        [TestMethod]
+        public void AttackMethodTesting1()
+        {
+            //Random number generator was changed to a hardcoded number for testing purposes
+            myClass.Prepare();
+
+            var Answer = myClass.Attack("3");
+
+            Assert.AreEqual("You have enliminated your enemy", Answer);
+        }
+
+        [TestMethod]
+        public void ScoreMethodTesting()
+        {
+            var Answer = myClass.Score("You have enliminated your enemy", "3");
+
+            Assert.AreEqual("4", Answer);
+        }
+
+        [TestMethod]
+        public void ScoreMethodTesting1()
+        {
+            var Answer = myClass.Score("Your attack failed", "3");
+
+            Assert.AreEqual("2", Answer);
+        }
+
+        [TestMethod]
+        public void HighScoreMethodTesting()
+        {
+            var Answer = myClass.HighScore("5", "3");
+
+            Assert.AreEqual("5", Answer);
+        }
+
+        [TestMethod]
+        public void HighScoreMethodTesting1()
+        {
+            var Answer = myClass.HighScore("2", "3");
+
+            Assert.AreEqual("3", Answer);
+        }
+
+    }
+}
